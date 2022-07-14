b706ff322d423f5ffaab3b002655dcd083ea0066
Felipe Ribeiro <fefeucn_cm@hotmail.com>
2022-07-14 00:16:59 -0300

    Consumo de uma API de eventos e correção de bugs nos filtros

--- src/state/seletors/index.ts
+++ src/state/seletors/index.ts
@@ -1,4 +1,5 @@
 import { selector } from "recoil";
+import { IEvento } from "../../interfaces/IEvento";
 import { filtroDeEventos, listaDeEventosState } from "../atom";
 
 const ehOMesmoDia = (primeiraData: Date, segundaData: Date) => {
@@ -10,16 +11,25 @@ export const eventosFiltradosState = selector({
   get: ({ get }) => {
     const filtro = get(filtroDeEventos)
     const todosOsEventos = get(listaDeEventosState)
-    console.log(filtro.status);
     const eventos = todosOsEventos.filter(evento => {
       if (!filtro.data) {
         if (filtro.status === "completo") return evento.completo
         if (filtro.status === "incompleto") return !evento.completo
         return true
       }
-      const status = filtro.status === "completo" ? true : false
-      return ehOMesmoDia(filtro.data, evento.inicio) && status;
+      if (filtro.status === "completo") return ehOMesmoDia(filtro.data, evento.inicio) && evento.completo === true;
+      else if (filtro.status === "incompleto") return ehOMesmoDia(filtro.data, evento.inicio) && evento.completo === false;
+      return ehOMesmoDia(filtro.data, evento.inicio);
     })
     return eventos;
+  }
+})
+
+export const eventosAsync = selector({
+  key: 'eventosAsync',
+  get: async () => {
+    const response = await fetch("http://localhost:8080/eventos");
+    const eventos: IEvento[] = await response.json();
+    return eventos.map(evento => ({ ...evento, inicio: new Date(evento.inicio), fim: new Date(evento.fim) }))
   }
 })
